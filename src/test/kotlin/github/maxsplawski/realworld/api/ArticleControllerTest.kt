008c359be12650b81d2e2994fc1da8d889bb52ef
Max Spławski <mniamek9@gmail.com>
2024-09-12 21:41:38 +0200

    feat: add Article controller, test

---
+++ src/test/kotlin/github/maxsplawski/realworld/api/ArticleControllerTest.kt
@@ -0,0 +1,21 @@
+package github.maxsplawski.realworld.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest(ArticleController::class)
+class ArticleControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return hello world`() {
+        mockMvc.get("/api/articles")
+            .andExpect { status { isOk() } }
+            .andExpect { content { string("Hello World") } }
+    }
+}
