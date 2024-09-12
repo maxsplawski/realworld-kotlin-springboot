e7eec4ef5206e4825612df49c37483b4d27d9132
Max Spławski <mniamek9@gmail.com>
2024-09-12 21:22:50 +0200

    init: initial commit

---
+++ src/test/kotlin/github/maxsplawski/realworld/RealWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.maxsplawski.realworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
